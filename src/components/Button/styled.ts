8c6422a386d0ea298f961099c88014901c12f862
HanSu Lee <i.hansu.lee@gmail.com>
2022-09-28 03:47:21 +0900

    feat: add `social` into type property of Button component

--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -2,19 +2,34 @@ import styled from '@emotion/styled';
 
 import { colors } from '../../styles';
 
-export const ButtonElement = styled.button({
+export const ButtonElement = styled.button<{
+  color?: string;
+  background?: string;
+  activeBackground?: string;
+  borderColor?: string;
+  fontWeight?: number;
+}>(({ color, background, activeBackground, borderColor, fontWeight }) => ({
   width: '100%',
   height: '4.8rem',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
   fontSize: '1.6rem',
-  fontWeight: 700,
-  color: '#FFFFFF',
-  background: colors.primary.default,
+  fontWeight: fontWeight || 700,
+  color: color || '#FFFFFF',
+  background: background || colors.primary.default,
   outline: 'none',
-  border: 'none',
+  border: borderColor ? `1px solid ${borderColor}` : 'none',
   borderRadius: '1.2rem',
   transition: 'background 100ms',
+  fontFamily: `'Spoqa Han Sans Neo', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+  Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`,
 
   '&:active': {
-    background: colors.primary.darker,
+    background: activeBackground || colors.primary.darker,
   },
-});
+
+  '& > img': {
+    marginRight: '1.2rem',
+  },
+}));
