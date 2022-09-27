ad82a87ccc6c4082b9b4ea22285fac109270d1cc
HanSu Lee <i.hansu.lee@gmail.com>
2022-09-28 02:06:55 +0900

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const App: React.FC = () => <div>한팀 QR</div>;
+import { MainPage } from './pages';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<MainPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const MainPage: React.FC = () => <div>한팀 QR</div>;
