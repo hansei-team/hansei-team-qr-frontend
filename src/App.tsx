74bc21eeaa66ae52769ebb62cc1c7ba6cbe4901a
HanSu Lee <i.hansu.lee@gmail.com>
2022-09-28 02:52:05 +0900

    feat: add emotion global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { Global } from '@emotion/react';
+
 import { MainPage } from './pages';
+import { globalStyle } from './styles';
 
 const App: React.FC = () => (
   <BrowserRouter>
+    <Global styles={globalStyle} />
     <Routes>
       <Route index element={<MainPage />} />
     </Routes>
---
+++ src/styles/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+  background: '#FFFFFF',
+  outline: '#EBEBEB',
+  error: '#FF4747',
+
+  primary: {
+    default: '#3E7FFF',
+    lighter: '#8EB5FF',
+  },
+
+  text: {
+    primary: '#000000',
+    secondary: '#7D8087',
+  },
+};
