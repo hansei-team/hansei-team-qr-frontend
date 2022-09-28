a5a07d2006f13dca63d96ab0c4716b3d1ef5902e
HanSu Lee <i.hansu.lee@gmail.com>
2022-09-28 09:27:18 +0900

    feat: add on-boarding(main) page markup

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,3 +1,22 @@
 import React from 'react';
 
-export const MainPage: React.FC = () => <div>한팀 QR</div>;
+import GooglePNG from '../../assets/google.png';
+import LogoPNG from '../../assets/logo.png';
+import { Button } from '../../components';
+
+import * as S from './styled';
+
+export const MainPage: React.FC = () => (
+  <S.PageContainer>
+    <S.OnBoardingItemContainer>
+      <S.LogoImage src={LogoPNG} alt="한팀" />
+      <div style={{ textAlign: 'center' }}>
+        <S.SmallText>한세어울림마당 x 한팀</S.SmallText>
+        <S.BrandText>한팀 QR</S.BrandText>
+      </div>
+    </S.OnBoardingItemContainer>
+    <Button type="social" socialType="GOOGLE" iconUrl={GooglePNG}>
+      구글로 시작하기
+    </Button>
+  </S.PageContainer>
+);
