51c17d2718f96b1723a8233dc2a7666b30bf7367
HanSu Lee <i.hansu.lee@gmail.com>
2022-09-28 11:24:14 +0900

    feat: add recoil

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { RecoilRoot } from 'recoil';
+
 import App from './App';
+import './firebase';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
