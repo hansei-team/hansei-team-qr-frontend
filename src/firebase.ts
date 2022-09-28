b9103fe2b1f77800cd5b3163ffe51793bb3f27d0
HanSu Lee <i.hansu.lee@gmail.com>
2022-09-28 15:00:06 +0900

    fix: change firebase configs

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC4vMGo5nVjEoFKMgYQQ1pz9SdFbuUD20I',
-  authDomain: 'hansei-team-qr.firebaseapp.com',
-  projectId: 'hansei-team-qr',
-  storageBucket: 'hansei-team-qr.appspot.com',
-  messagingSenderId: '1024315752333',
-  appId: '1:1024315752333:web:ce446c43c6a93fb2e247c4',
-  measurementId: 'G-P7J2L2TYR3',
+  apiKey: 'AIzaSyDyBxx2Bm6Wm0t-lsYoeKeroa_tipMPHY0',
+  authDomain: 'hansei-team-qr-b59ea.firebaseapp.com',
+  projectId: 'hansei-team-qr-b59ea',
+  storageBucket: 'hansei-team-qr-b59ea.appspot.com',
+  messagingSenderId: '189783632613',
+  appId: '1:189783632613:web:734982b91b9b884cca020c',
+  measurementId: 'G-NMJ6ZGMLCM',
 };
 
 export const app = initializeApp(firebaseConfig);
