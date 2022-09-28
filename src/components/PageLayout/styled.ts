0c85089e317ea9492d5701bcb6e0ae976616b0a9
HanSu Lee <i.hansu.lee@gmail.com>
2022-09-28 13:17:47 +0900

    feat: add PageLayout component

---
+++ src/components/PageLayout/styled.ts
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const PageLayoutContainer = styled.div({
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '3.2rem 2.4rem',
+});
+
+export const IconTitle = styled.img({
+  width: '5.5rem',
+  height: '5.5rem',
+});
+
+export const PageContainer = styled.main({
+  flex: '1 1 auto',
+});
+
+export const PageTitle = styled.h1({
+  fontSize: '2.4rem',
+  fontWeight: 700,
+  lineHeight: '3rem',
+  marginTop: '3.6rem',
+});
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './Button';
+export * from './PageLayout';
 export * from './TextField';
