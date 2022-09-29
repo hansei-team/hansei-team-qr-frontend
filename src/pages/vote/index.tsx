1df9a654e63c5f11664e985ea8d57290a541b279
HanSu Lee <i.hansu.lee@gmail.com>
2022-09-30 03:47:51 +0900

    feat: add vote logics

--- src/pages/vote/index.tsx
+++ src/pages/vote/index.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
-import { onValue, ref } from 'firebase/database';
+import { onValue, ref, set } from 'firebase/database';
 
 import SuccessPNG from '../../assets/emoji/success.png';
 import { Button, EmojiCard, PageLayout } from '../../components';
-import { database } from '../../firebase';
+import { auth, database } from '../../firebase';
+import { useRequest } from '../../hooks';
 
 import * as S from './styled';
 
@@ -13,14 +15,57 @@ interface Vote {
   openAt: string;
 }
 
+interface VoteOptions {
+  key: string;
+  order: number;
+  text: [string, string];
+}
+
 export const VotePage: React.FC = () => {
   const [select, setSelect] = useState<string | null>(null);
   const [vote, setVote] = useState<Vote>({ isOpen: false, openAt: '' });
+  const [isAlreadyVoted, setIsAlreadyVoted] = useState<boolean>(false);
+  const [voteOptions, setVoteOptions] = useState<VoteOptions[]>([]);
   const BASE64_DECODER_URL = 'https://www.convertstring.com/ko/EncodeDecode/Base64Decode';
+  const voteRef = ref(database, 'vote');
+  const voteOptionsRef = ref(database, 'voteOptions');
+  const voteResultsRef = ref(database, 'voteResults');
+
+  const { mutate } = useRequest(
+    async ({ uid, selected }: { uid: string; selected: string }) => {
+      await set(ref(database, `voteResults/${uid}`), {
+        selected,
+        submittedAt: new Date().getTime(),
+      });
+
+      return true;
+    },
+    {
+      onSuccess: () => {
+        toast.success('투표 제출이 완료되었어요!');
+      },
+      onError: (error) => {
+        toast.error('투표 제출 중 오류가 발생했어요');
+      },
+    }
+  );
 
   const handleOnClickVoteOption = (optionId: string) => {
+    if (isAlreadyVoted) return;
+
     if (optionId === select) setSelect(null);
     else setSelect(optionId);
+  };
+
+  // eslint-disable-next-line consistent-return
+  const handleOnSubmitVote = async () => {
+    if (!voteOptions.find((option) => option.key === select) || select === null)
+      return toast.error('올바른 가수를 선택해주세요');
+    if (!auth.currentUser) {
+      return toast.error('로그인 후 이용해주세요');
+    }
+
+    mutate({ uid: auth.currentUser.uid, selected: select });
   };
 
   const getEmojiProps = (optionId: string) => {
@@ -33,10 +78,38 @@ export const VotePage: React.FC = () => {
   };
 
   useEffect(() => {
-    const voteRef = ref(database, 'vote');
-    const unsubscribe = onValue(voteRef, (snapshot) => setVote(snapshot.val()));
+    if (!auth.currentUser) return () => {};
 
-    return () => unsubscribe();
+    const unsubscribeVote = onValue(voteRef, (snapshot) => setVote(snapshot.val()));
+    const unsubscribeVoteOptions = onValue(voteOptionsRef, (snapshot) => {
+      const value = snapshot.val();
+      const options = Object.keys(value)
+        .reduce((prev: VoteOptions[], curr: string) => [...prev, { key: curr, ...value[curr] }], [])
+        .sort((a, b) => a.order - b.order);
+
+      setVoteOptions(options);
+    });
+
+    const unsubscribeMyVote = onValue(
+      ref(database, `voteResults/${auth.currentUser.uid}`),
+      (snapshot) => {
+        const voteData = snapshot.val();
+
+        if (voteData) {
+          setIsAlreadyVoted(true);
+          setSelect(voteData.selected);
+        } else {
+          setIsAlreadyVoted(false);
+          setSelect(null);
+        }
+      }
+    );
+
+    return () => {
+      unsubscribeVote();
+      unsubscribeVoteOptions();
+      unsubscribeMyVote();
+    };
   }, []);
 
   if (!vote.isOpen)
@@ -59,20 +132,21 @@ export const VotePage: React.FC = () => {
           당신의 가수를 선택해주세요
         </PageLayout.Title>
         <S.VoteOptionsContainer>
-          <EmojiCard
-            text={['교사팀', '홍길동 선생님 / 고길동 선생님']}
-            onClick={() => handleOnClickVoteOption('teacher')}
-            {...getEmojiProps('teacher')}
-          />
-          <EmojiCard
-            text={['학생팀', 'C1111 바나나 / H1111 사과']}
-            onClick={() => handleOnClickVoteOption('student')}
-            {...getEmojiProps('student')}
-          />
+          {voteOptions.map(({ text, key }) => (
+            <EmojiCard
+              key={key}
+              text={text}
+              onClick={() => handleOnClickVoteOption(key)}
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...getEmojiProps(key)}
+            />
+          ))}
         </S.VoteOptionsContainer>
         <S.VoteDescriptionText>※ 한 번 제출하면 다시 수정할 수 없어요!</S.VoteDescriptionText>
       </div>
-      <Button disabled={select === null}>투표 제출하기</Button>
+      <Button onClick={handleOnSubmitVote} disabled={select === null || isAlreadyVoted}>
+        {isAlreadyVoted ? '이미 투표를 제출했어요' : '투표 제출하기'}
+      </Button>
     </S.VotePageContainer>
   );
 };
