8dfc9b3bf2a9f474b815a6cc0cc0fe39dbab5811
HanSu Lee <i.hansu.lee@gmail.com>
2022-09-29 23:44:30 +0900

    refactor: change firebase config values to env variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,14 +5,14 @@ import { getDatabase } from 'firebase/database';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDyBxx2Bm6Wm0t-lsYoeKeroa_tipMPHY0',
-  authDomain: 'hansei-team-qr-b59ea.firebaseapp.com',
-  projectId: 'hansei-team-qr-b59ea',
-  storageBucket: 'hansei-team-qr-b59ea.appspot.com',
-  messagingSenderId: '189783632613',
-  appId: '1:189783632613:web:734982b91b9b884cca020c',
-  measurementId: 'G-NMJ6ZGMLCM',
-  databaseURL: 'https://hansei-team-qr-b59ea-default-rtdb.asia-southeast1.firebasedatabase.app/',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 };
 
 export const app = initializeApp(firebaseConfig);
